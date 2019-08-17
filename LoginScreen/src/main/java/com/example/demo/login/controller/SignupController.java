bf3a6ac1486a4bf4f67136083772af44e0e1eb47
kuboshoji <jiangzhijiubao@gmail.com>
2019-08-17 11:19:40 +0900

    エラー共通画面の表示

--- LoginScreen/src/main/java/com/example/demo/login/controller/SignupController.java
+++ LoginScreen/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -4,10 +4,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +36,37 @@ public class SignupController {
 		
 	return radio;	
 }
+	//@ExceptionHandlerの使い方
+	@ExceptionHandler(DataAccessException.class)
+	public String dataAccessExceptionHandler(DataAccessException e, Model model) {
+		
+		//例外クラスのメッセージをModelに登録
+		model.addAttribute("error");
+		
+		//例外クラスのメッセージをModelに登録
+		model.addAttribute("message","SignupController で DataAccessException が発生しました");
+		
+		//HTTPのエラーコード(500)をModelに登録
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return "error";
+		
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e, Model model) {
+		
+		//　例外クラスのメッセージをModelに登録
+		model.addAttribute("error","内部サーバーエラー : ExceptionHandler");
+		
+		//例外クラスのメッセージをModelに登録
+		model.addAttribute("message","SignupController で Exception が発生しました");
+		
+		//HTTPのエラーコード(500)をModelに登録
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return "error";
+	}
 
 //ポイント１：@ModelAttribute
 //ユーザー登録画面のGET用コントローラー	@Validated指定の問題
