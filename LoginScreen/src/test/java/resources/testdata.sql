033239ebb5fa60874d06ca2217896edaa735e32a
kuboshoji <jiangzhijiubao@gmail.com>
2019-08-28 21:24:10 +0900

    画面表示内容のtesuto

---
+++ LoginScreen/src/test/java/resources/testdata.sql
@@ -0,0 +1,3 @@
+/* テスト用のデータ(ADMIN 権限) */
+INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
+VALUES('satou@xxx.co.jp','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIpa','佐藤一郎','1990-01-01',28,false,'POLE_ADMIN');
